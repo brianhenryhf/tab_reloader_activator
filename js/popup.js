cb371cf9afead0f51cd268372203f4d7aae17ebf
Brian Henry <brian.henry@ideacrew.com>
2024-11-12 19:27:10 -0700

    update buttons with state of reloadingness

--- js/popup.js
+++ js/popup.js
@@ -2,14 +2,21 @@ import { getCurrentTab } from './utils.js';
 import { logToUi } from './dev_utils.js';
 
 
-// TODO basically functional - check in
-// TODO - buttons that know if tab is reloading.
 // TODO - some other indicator of reloading state
-// TOOD options for reload interval
+// TODO options for reload interval, of course
 
+// TODO some way to kill alarm if tab is closed? so not extra processing, but also so that's a way to kill the reload. currenlty
+// yeah, these alarms keep going. i thin if tabs are closed, the alarms will keep firing... bleah.
+// Tab#onRemoved (i.e. closed.  perhaps also Tab#onUpdated where changeInfo.url has a val. but latter might have unexpected effects. maybe sanest to just keep reload in effect until closed.
+// fine point: this is then tab reloader, not site or doc reloader...
 
-// TOOD no-discard fucntionality - priritize this!! the above is important, but don't care prioritywise
+// TODO refresh popup when tab is reloaded or nav'd to another location... button states stay same. does tabid stay the same? bug or feature?
 
+// note - well, i guess duh, but reloading a tab doesn't stop it from reloading (alarm is still active, tab id is not changed)
+// surprising tho - if i close and paste url i a new tab, this is still reloading..
+// TODO RECONFIRM
+
+// TODO no-discard fucntionality - priritize this!! the above is important, but don't care prioritywise
 
 
 
@@ -19,14 +26,24 @@ const RELOAD_MINS_TEMP = 0.5;
 (() => {
   'use strict';
 
+  // for now, we assume only reload alarms are important to us. later, we might adjust naming or make more geenric and bind to something specific or whatever.
+
   // yeah, regex stuff is fiddly, filter it out.
   const tabAlarmName = (tab) => `tabReload-${tab.id}`;
 
+  /** is a particular alarm relevant to a given tab */
   const isAlarmForTab = (alarm, tab) => alarm.name === tabAlarmName(tab);
+
+  /** does a tab have any alarm we care about (w/r/t reloading)? */
+  // Async! change naming to clarify? b/c always truthy... blasted red/blue problem made worse with weak typing.
+  const isTabAlarmed = async (tab) => {
+    const alarms = await chrome.alarms.getAll();
+    return alarms.some(alarm => isAlarmForTab(alarm, tab));
+  };
 
   const startReloadTab = async (tab) => {
     // shortcircuit if dupe request on same tab
-    if((await chrome.alarms.getAll()).some(alarm => isAlarmForTab(alarm, tab))) return;
+    if(await isTabAlarmed(tab)) return;
 
     // oh, this takes a callback.  but this is just be firing once, immediately. not helpful.
     await chrome.alarms.create(tabAlarmName(tab), { periodInMinutes: RELOAD_MINS_TEMP });
@@ -35,13 +52,13 @@ const RELOAD_MINS_TEMP = 0.5;
       // logToUi(`FIRED event ${alarm.name} for tab ${tabId}`);
       // interestingly, the id type matters for sig match. Um, aren't we doing javascript here?
       if(isAlarmForTab(alarm, tab)) {
-          console.log('reloading!')
+        console.log(`reloading! b/c ${alarm.name}`)
         chrome.tabs.reload(tab.id);
-      } else console.log('nope')
+      } else console.log(`nope, not ${alarm.name}`);
 
     });
 
-    // is there anything we can do to show we're reloading a given tab? i suspect not, but look into. i guess can highlight the extension button. but prolly can't mess witht he tab.
+    // is there anything we can do to show we're reloading a given tab? i suspect limited, but look into. i guess can highlight the extension button. but prolly can't mess witht he tab.
   };
 
 
@@ -61,59 +78,38 @@ const RELOAD_MINS_TEMP = 0.5;
   };
 
 
-  // TODO bring this back in.  indicator of reloadingness is needed.
-
-  // const toggleSuspend = async (tab) => {
-  //   if(isSuspendedPageUrl(tab.url)) {
-  //     return unsuspendTab(tab);
-  //   } else {
-  //     if(isSuspendableUrl(tab.url)) {
-  //       return suspendTab(tab);
-  //     }
-  //   }
-  // };
-
-
-  // const updateButtonStates = (currentTab, windowTabs) => {
-  //   log('here')
-  //   // a page that is in process of loading the placeholder page may have "status":"loading" and a value for pendingUrl reflecting the new url.
-  //   // MAY be true for a page just starting to load as well - haven't confirmed, but would make sense
-  //   suspendCurrentBtn.disabled = !isSuspendableUrl(currentTab.pendingUrl || currentTab.url);
-  //   unsuspendCurrentBtn.disabled = !isSuspendedPageUrl(currentTab.pendingUrl || currentTab.url);
-  //
-  //   //rare case, but if _all_ tabs are not suspendable, then the button is disabled.  if any of them is, suspend button is usable.  'all' term should probably be 'any'.
-  //   suspendAllBtn.disabled = windowTabs.every(it => !isSuspendableUrl(it.pendingUrl || it.url));
-  //   unsuspendAllBtn.disabled = !windowTabs.some(it => isSuspendedPageUrl(it.pendingUrl || it.url));
-  // };
-
-
   let reloadCurrentBtn;
   let stopCurrentBtn;
 
+  const updateButtonStates = async (currentTab) => {
+    reloadCurrentBtn.disabled = await isTabAlarmed(currentTab);
+    stopCurrentBtn.disabled = !reloadCurrentBtn.disabled;
+  };
+
+
   window.addEventListener('DOMContentLoaded', async (event) => {
-    // log('popup loaded');
     reloadCurrentBtn = document.querySelector('#reload-btn');
     stopCurrentBtn = document.querySelector('#stop-btn');
 
     const currentTab = await getCurrentTab();
-    // log(tab)
+    // console.dir(currentTab)
 
     reloadCurrentBtn.querySelector('#reloadInterval').textContent = RELOAD_MINS_TEMP;
 
     reloadCurrentBtn.addEventListener('click', async () => {
-      startReloadTab(currentTab);
+      await startReloadTab(currentTab);
+      await updateButtonStates(currentTab);
     });
 
     stopCurrentBtn.addEventListener('click', async () => {
-      stopReloadTab(currentTab);
+      await stopReloadTab(currentTab);
+      await updateButtonStates(currentTab);
     });
-
-
 
     //yeah prolly need to keep track of what is reloading and update buttons. capture where? in the hash? in storage? (but what happens if they leave and come back to the tab and don't expect it to be reloading still?_
     // state in the hash seems easier in that way. but we'r enot gonna poll every tab everywher to see if we're to reload. i mean if there's some ephemeral way to associate a timer to a tab... an id?
     //  yeah there's an id that's only good for a session. and you can re-get tabs by it. that's the cheese.
 
-    // updateButtonStates(currentTab, windowTabs);
+    await updateButtonStates(currentTab);
   });
 })();
